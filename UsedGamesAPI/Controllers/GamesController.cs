a22f4f0b7d13a82132f357c1812b433953adfb63
Matheus Peixoto <matheuspeixoto100@gmail.com>
2021-04-11 15:37:54 -0300

    Adicionado controller GamesController

---
+++ UsedGamesAPI/Controllers/GamesController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsedGamesAPI.Repositories.Interfaces;
+
+namespace UsedGamesAPI.Controllers
+{
+    [ApiController]
+    [Route("games")]
+    public class GamesController : ControllerBase
+    {
+        private readonly IGameRepository _gameRepository;
+        private readonly IMapper _mapper;
+
+        public GamesController(IGameRepository gameRepository, IMapper mapper)
+        {
+            _gameRepository = gameRepository;
+            _mapper = mapper;
+        }
+    }
+}
--- UsedGamesAPI/Startup.cs
+++ UsedGamesAPI/Startup.cs
@@ -29,6 +29,7 @@ namespace UsedGamesAPI
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<IClientRepository, ClientRepository>();
             services.AddScoped<ISellerRespository, SellerRepository>();
+            services.AddScoped<IGameRepository, GameRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
