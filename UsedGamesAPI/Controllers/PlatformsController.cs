405b935aed202ce8d7fc83d31c8ee1dee4dbc6cc
Matheus Peixoto <matheuspeixoto100@gmail.com>
2021-04-12 10:17:48 -0300

    Adicionado controller PlatformsController

---
+++ UsedGamesAPI/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UsedGamesAPI.Repositories.Interfaces;
+
+namespace UsedGamesAPI.Controllers
+{
+    [ApiController]
+    [Route("platforms")]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepository _platformRepository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepository platformRepository, IMapper mapper)
+        {
+            _platformRepository = platformRepository;
+            _mapper = mapper;
+        }
+    }
+}
