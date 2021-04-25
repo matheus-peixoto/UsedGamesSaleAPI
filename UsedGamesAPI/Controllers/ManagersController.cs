660ad19aa3a4c4e755c8758137d960e9e533e714
Matheus Peixoto <matheuspeixoto100@gmail.com>
2021-04-25 15:49:40 -0300

    Adicionado métodos Get ao controller Managers

--- UsedGamesAPI/Controllers/ManagersController.cs
+++ UsedGamesAPI/Controllers/ManagersController.cs
@@ -7,18 +7,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using UsedGamesAPI.DTOs.Users;
 using UsedGamesAPI.Models;
+using UsedGamesAPI.Models.Enums;
 using UsedGamesAPI.Repositories.Interfaces;
 using UsedGamesAPI.Services;
 
 namespace UsedGamesAPI.Controllers
 {
     [ApiController]
+    [Authorize(Roles = "Manager")]
     [Route("managers")]
-    public class ManagerController : ControllerBase
+    public class ManagersController : ControllerBase
     {
         private readonly IManagerRepository _managerRepository;
 
-        public ManagerController(IManagerRepository managerRepository)
+        public ManagersController(IManagerRepository managerRepository)
         {
             _managerRepository = managerRepository;
         }
@@ -31,9 +33,27 @@ namespace UsedGamesAPI.Controllers
             Manager manager = await _managerRepository.FindByAccountAsync(userDTO.Email, userDTO.Password);
             if (manager.IsNull()) return NotFound(new { message = "Invalid user or password" });
 
-            string token = TokenService.GenerateToken(manager);
+            string token = TokenService.GenerateToken(manager, AccountType.Manager);
             manager.Password = "";
             return Ok(new { manager, token });
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Manager>>> Get()
+        {
+            List<Manager> managers = await _managerRepository.FindAllAsync();
+            return Ok(managers);
+        }
+
+        [HttpGet]
+        [Route("{id:int}", Name = "GetManagerById")]
+        public async Task<ActionResult<Manager>> GetById([FromRoute] int id)
+        {
+            Manager manager = await _managerRepository.FindByIdAsync(id);
+            if (manager.IsNull()) return NotFound();
+
+            return Ok(manager);
         }
     }
 }
